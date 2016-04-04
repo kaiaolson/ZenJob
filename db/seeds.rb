f3f78203961fde259a11098090f82584a19327f1
kaiaolson <kaiaolson@gmail.com>
2016-04-03 20:16:05 -0700

    minor changes to navigation, build seed file with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+["kaia@mintakadesign.com", "test@test.com", "kaiaolsondebug@gmail.com", "testing@testing.com"].each do |email|
+  User.create(email: email,
+              password: "password",
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              role: "Consultant")
+end
+
+["tester@tester.com", "hello@test.com", "world@test.com", "helloworld@test.com"].each do |email|
+  User.create(email: email,
+              password: "password",
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              role: "Client")
+end
